59f0911a6d78efbf5e14c392abf1f336414e2899
LeMasta <127185197+LeMasta@users.noreply.github.com>
2026-09-20 16:31:10 +0800

    fix: unify interactive component feedback

--- src/main.tsx
+++ src/main.tsx
@@ -14,5 +14,6 @@ import './styles-v10.css'
 import './styles-v11.css'
 import './styles-v12.css'
 import './styles-v13.css'
+import './styles-v14.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>)
